7e7790418ebac4f84a42ddc8f16d29d16f48d4d5
Marcel van den Berg <34321907+mbe81@users.noreply.github.com>
2023-01-27 10:26:13 +0100

    fix: prevent undefined array warning

--- php/bulk_nslookup.php
+++ php/bulk_nslookup.php
@@ -5,7 +5,7 @@ $domains = explode("\n", <<<EOF
 EOF
 );
 
-echo "domainname\tip-address\tnameserver 1\tnameserver 2\tmailserver\n";
+echo "domainname;ip-address;nameserver 1;nameserver 2;mailserver\n";
 foreach ($domains as $domain) {
 
     $domain = trim($domain);
@@ -14,10 +14,10 @@ foreach ($domains as $domain) {
     $ns_record = dns_get_record($domain, DNS_NS);
     $mx_record = dns_get_record($domain, DNS_MX);
 
-    $ip_address = $a_record[0] ? $a_record[0]['ip'] : '';
-    $nameserver1 = $ns_record[0] ? $ns_record[0]['target'] : '';
-    $nameserver2 = $ns_record[1] ? $ns_record[1]['target'] : '';
-    $mailserver = $mx_record[0] ? $mx_record[0]['target'] : '';
+    $ip_address = $a_record && $a_record[0] ? $a_record[0]['ip'] : '';
+    $nameserver1 = $ns_record && $ns_record[0] ? $ns_record[0]['target'] : '';
+    $nameserver2 = $ns_record && $ns_record[1] ? $ns_record[1]['target'] : '';
+    $mailserver = $mx_record && $mx_record[0] ? $mx_record[0]['target'] : '';
 
-    echo strtolower($domain . "\t" . $ip_address . "\t" . $nameserver1 . "\t" . $nameserver2 . "\t" . $mailserver . "\n");
+    echo strtolower($domain . ";" . $ip_address . ";" . $nameserver1 . ";" . $nameserver2 . ";" . $mailserver . "\n");
 }
